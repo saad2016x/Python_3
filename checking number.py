99c38a026a30b4e2652f007804625043cb63a40b
SAAD BAKKIT ALSHARIF <38724825+saad2016x@users.noreply.github.com>
2020-04-07 21:21:07 +0300

    Add files via upload

---
+++ checking number.py
@@ -0,0 +1,8 @@
+s = raw_input("Enter a number : ")
+
+if int(s) < 10 :
+    print "Number is less than 10"
+elif int(s) == 10 :
+    print "Number is equal to 10"
+else :
+    print "Number is greater than 10"
